b4f7099fdba3f8df325f7a2d71e1921ab13a72da
toydotgame <toydotgame@gmail.com>
2025-03-28 00:00:32 +1000

    Changed scope of imgmodal from `var`s to `let`

--- imgmodal.js
+++ imgmodal.js
@@ -5,16 +5,15 @@
  */
 
 document.getElementById("content").innerHTML += '<div id="modaldim" style="display:none;"></div>';
-var overlay = document.getElementById("modaldim");
+let overlay = document.getElementById("modaldim");
 overlay.addEventListener("click", onClick);
 
-var imgs = document.getElementById("content").children[0].getElementsByTagName("img");
-for(var i = 0; i < imgs.length; i++) {
+let imgs = document.getElementById("content").children[0].getElementsByTagName("img");
+for(let i = 0; i < imgs.length; i++)
 	imgs[i].addEventListener("click", onClick);
-}
 
 overlay.innerHTML = '<img id="modalimg">';
-var img = document.getElementById("modalimg");
+let img = document.getElementById("modalimg");
 
 function onClick(e) {
 	img.src = e.target.src;
